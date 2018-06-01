652d3ae0129d9afe67461a96cfbdc1e7a0e909ec
bixinkun <bxk0724@163.com>
2018-06-01 17:39:46 +0800

    保存

---
+++ com.bxk/src/算法3.java
@@ -0,0 +1,18 @@
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * @Author：Bixinkun
+ * @Descripition: 输入一串字符串，打印出字符串的所有排列
+ * @Date:Created in 2018-06-01 17:19
+ */
+public class 算法3 {
+
+    public static void main(String[] args) {
+        InputStream in = System.in;
+
+
+        System.out.println();
+    }
+
+}
